0a1fc8e2b303a822c03b396184cd7898622495f1
hmimeee <hmimeee@gmail.com>
2021-09-22 13:23:03 +0600

    Cached for extra query

--- start.php
+++ start.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Modules\Article\Entities\ArticleSetting;
 
 /*
@@ -15,3 +16,34 @@ use Modules\Article\Entities\ArticleSetting;
 */
 
 require __DIR__ . '/Routes/web.php';
+
+function article_settings()
+{
+    if (!cache()->has('article_settings')) {
+        $outreachHead = ArticleSetting::where('type', 'outreach_head')->first()->value ?? '';
+        $outreachAssistants = ArticleSetting::where('type', 'outreach_assistants')->first()->value ?? '';
+        $writerHead = ArticleSetting::where('type', 'writer_head')->first()->value ?? '';
+        $writerHeadAssistant = ArticleSetting::where('type', 'writer_head_assistant')->first()->value ?? '';
+        $publisher = ArticleSetting::where('type', 'publisher')->first()->value ?? '';
+        $publishers = ArticleSetting::where('type', 'publishers')->first()->value ?? '';
+        $writerRole = ArticleSetting::where('type', 'writer')->first()->value ?? '';
+        $inhouseWriterRole = ArticleSetting::where('type', 'inhouse_writer')->first()->value ?? '';
+
+        $data = (object) [
+            'outreachHead' => $outreachHead,
+            'outreachAssistants' => $outreachAssistants,
+            'writerHead' => $writerHead,
+            'writerHeadAssistant' => $writerHeadAssistant,
+            'publisher' => $publisher,
+            'publishers' => $publishers,
+            'writerRole' => $writerRole,
+            'inhouseWriterRole' => $inhouseWriterRole,
+        ];
+
+        cache()->put('article_settings', $data, 10080);
+    } else {
+        $data = cache()->get('article_settings');
+    }
+
+    return $data;
+}
